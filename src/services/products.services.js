6f0fb514015b9dab1f4df53c55da9a09688a3f63
Ana Luiza Fernandes <analuizateixeirafernandes184@gmail.com>
2023-04-04 15:46:17 -0300

    feat: get product details on route GET /products/:id

--- src/services/products.services.js
+++ src/services/products.services.js
@@ -1,4 +1,6 @@
+import { ObjectId } from "mongodb";
 import { productsRepository } from "../repositories/products.repository.js";
+import { categoriesRepository } from "../repositories/categories.repository.js";
 
 async function listAll() {
 	const products = await productsRepository.findAll();
@@ -13,6 +15,18 @@ async function listAll() {
 	return formattedProducts;
 }
 
-const productsService = { listAll };
+async function findById(id) {
+	const product = await productsRepository.findById(id);
+	if (product === null) throw new Error("Not Found");
+
+	const category = await categoriesRepository.findById(product.categoryId);
+	if (category === null) throw new Error("Not Found");
+
+	const formattedProduct = { ...product, categoryName: category.name };
+
+	return formattedProduct;
+}
+
+const productsService = { listAll, findById };
 
 export { productsService };
--- tests/unit/products.services.spec.js
+++ tests/unit/products.services.spec.js
@@ -1,12 +1,13 @@
 import { jest } from "@jest/globals";
 import { productsService } from "../../src/services/products.services.js";
 import { productsRepository } from "../../src/repositories/products.repository.js";
+import { categoriesRepository } from "../../src/repositories/categories.repository.js";
 
 describe("products service unit test swite", () => {
 	it("should return an empty array when there is no product to list", async () => {
 		jest
 			.spyOn(productsRepository, "findAll")
-			.mockImplementationOnce(() => new Promise((resolve) => resolve([])));
+			.mockImplementationOnce(() => Promise.resolve([]));
 
 		const result = await productsService.listAll();
 
@@ -40,12 +41,74 @@ describe("products service unit test swite", () => {
 
 		jest
 			.spyOn(productsRepository, "findAll")
-			.mockImplementationOnce(
-				() => new Promise((resolve) => resolve([product]))
-			);
+			.mockImplementationOnce(() => Promise.resolve([product]));
 
 		const result = await productsService.listAll();
 
 		expect(result).toEqual([formattedProduct]);
+	});
+
+	it("should return an error when there is no product with given id", () => {
+		jest
+			.spyOn(productsRepository, "findById")
+			.mockImplementationOnce(() => Promise.resolve(null));
+
+		const result = productsService.findById("123");
+
+		expect(result).rejects.toBeInstanceOf(Error);
+	});
+
+	it("should return an error when there is no category for product with given id", () => {
+		const product = {
+			_id: "123",
+			name: "product_name",
+			description: "product_description",
+			image: "https://product_name.jpg",
+			categoryId: "12345",
+			originalPrice: 2500,
+			isPromotion: false,
+			promotionPrice: null,
+			updatedAt: 12345,
+			createdAt: 67890,
+		};
+
+		jest
+			.spyOn(productsRepository, "findById")
+			.mockImplementationOnce(() => Promise.resolve(product));
+
+		jest
+			.spyOn(categoriesRepository, "findById")
+			.mockImplementationOnce(() => Promise.resolve(null));
+
+		const result = productsService.findById("123");
+
+		expect(result).rejects.toBeInstanceOf(Error);
+	});
+
+	it("should return the product with given id formatted", async () => {
+		const product = {
+			_id: "123",
+			name: "product_name",
+			description: "product_description",
+			image: "https://product_name.jpg",
+			categoryId: "12345",
+			originalPrice: 2500,
+			isPromotion: false,
+			promotionPrice: null,
+			updatedAt: 12345,
+			createdAt: 67890,
+		};
+
+		jest
+			.spyOn(productsRepository, "findById")
+			.mockImplementationOnce(() => Promise.resolve(product));
+
+		jest
+			.spyOn(categoriesRepository, "findById")
+			.mockImplementationOnce(() => Promise.resolve({ name: "category" }));
+
+		const result = await productsService.findById();
+
+		expect(result).toEqual({ ...product, categoryName: "category" });
 	});
 });
